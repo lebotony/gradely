c7dd3e385d0f2d9eb1b6fd3e9528d7407be62d79
lebohang <lebomdlongwa@gmail.com>
2025-06-29 00:05:09 +0200

    fix: typescript types and migration errors

--- assets/js/components/colors.ts
+++ assets/js/components/colors.ts
@@ -1,20 +1,3 @@
-type ColorProps = {
-  grayLighter: "#e0e0e0";
-  gray: "gray";
-  white: "white";
-  black: "black";
-  lightgray: "lightgray";
-  iconGray: "#989898";
-  chalk: "#eef0f4";
-  lightBlack: "#333";
-  red: "#FF0000";
-  green: "#2ECC71";
-  darkCyan: "#26A69A";
-  darkModerateBlue: "#3949AB";
-  neonRed: "#FF3131";
-  fontBlack: "#28282B";
-};
-
 export const Colors: ColorProps = {
   grayLighter: "#e0e0e0",
   white: "white",
--- priv/repo/migrations/20250623000500_create_classes.exs
+++
@@ -1,9 +0,0 @@
-defmodule Gradely.Repo.Migrations.CreateClasses do
-  use Ecto.Migration
-
-  def change do
-    create table(:classes) do
-      timestamps()
-    end
-  end
-end
